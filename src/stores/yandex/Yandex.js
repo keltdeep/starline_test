e3e6073badeee094211210d9226ad6dfc7c9d753
keltdeep <keltdeep@gmail.com>
2022-04-22 07:02:02 +0300

    addComments

--- src/stores/yandex/Yandex.js
+++ src/stores/yandex/Yandex.js
@@ -30,10 +30,12 @@ class YandexStore {
 
       this.setRestaurants(restourants?.data.features);
     } catch(err) {
+      //тут бы нотификацию
       console.log(err);
     }
   }
 
+  //ключ в енв выносить надо, т.к. тестовое не стал делать
   getRestaurantsByApi = (skip) =>
     axios.get(`https://search-maps.yandex.ru/v1/?text=москва рестораны&results=15&skip=${skip}&type=biz&lang=ru_RU&apikey=9e8e710b-8281-4f3a-a272-5dadd59ef0fd`,
       {

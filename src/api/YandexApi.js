a86385b683fb6d04e206aa43a4faea6d86a71b66
keltdeep <keltdeep@gmail.com>
2022-04-23 07:56:03 +0300

    refactor

--- src/api/YandexApi.js
+++ src/api/YandexApi.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_YANDEX_KEY = 'ВставьтеВашКлюч'
+
 export const YandexApi = (skip) =>
-  axios.get(`https://search-maps.yandex.ru/v1/?text=москва рестораны&results=15&skip=${skip}&type=biz&lang=ru_RU&apikey=ВставьтеВашКлюч`,
+  axios.get(`https://search-maps.yandex.ru/v1/?text=москва рестораны&results=15&skip=${skip}&type=biz&lang=ru_RU&apikey=${API_YANDEX_KEY}`,
     {
       headers: {
         'Content-Type': 'application/json'

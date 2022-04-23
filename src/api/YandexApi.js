3e225bac87c99604e5da1282798e49d1c65704e2
keltdeep <keltdeep@gmail.com>
2022-04-23 08:48:06 +0300

    addCOmments

--- src/api/YandexApi.js
+++ src/api/YandexApi.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+// ключ в енв вынести
 const API_YANDEX_KEY = 'ВставьтеВашКлюч'
 
 export const YandexApi = (skip) =>
+  // к апишкам лучше с бека обращаться
   axios.get(`https://search-maps.yandex.ru/v1/?text=москва рестораны&results=15&skip=${skip}&type=biz&lang=ru_RU&apikey=${API_YANDEX_KEY}`,
     {
       headers: {
